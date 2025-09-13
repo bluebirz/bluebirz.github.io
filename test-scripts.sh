b47a1e7dd92240da8299fc7c53b4ea00530b2a49
bluebirz <9207891+bluebirz@users.noreply.github.com>
2025-09-13 19:11:10 +0200

    improve pre-commit + fix frontmatter

--- test-scripts.sh
+++
@@ -1,9 +0,0 @@
-bundle exec htmlproofer _site \
-  \-\-disable-external \
-  \-\-ignore-urls "/^http:\/\/127.0.0.1/,/^http:\/\/0.0.0.0/,/^http:\/\/localhost/"
-{
-  echo "Checking relative path of media..."
-  rg -i --pcre2 "(?<!lqip):\s*\.+\/" _posts/ --sort-files && exit 1
-} || {
-  echo "no relative path found. :)"
-}
